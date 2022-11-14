2d38f27f36878ef4b5ffd1efef4008ae00cb9ece
Violetta Iuzykhovich <violetta100100100@gmail.com>
2022-11-14 19:29:45 +0300

    1 commit

---
+++ src/test/java/org/veta/WebDriverTest.java
@@ -0,0 +1,12 @@
+package org.veta;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class WebDriverTest {
+    public void testGithub() {
+        WebDriver driver = new ChromeDriver();
+
+    }
+
+}
